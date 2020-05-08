4c792ce4d3543d867bdfaf620b7910b5605141a6
harshit <harshitsharmajs@gmail.com>
2020-05-07 17:26:21 -0700

    0104

---
+++ C++/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+// Time O(n)
+// Space O(h) where h is the height of tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root){
+            return 0;
+        }
+        return max( maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
