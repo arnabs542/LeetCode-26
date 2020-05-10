318e2edfa969a6d7e4fdcd0c756b85bc1391afcc
harshit <harshitsharmajs@gmail.com>
2020-05-10 13:12:06 -0700

    0102

---
+++ C++/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// Time:  O(n)
+// Space: O(n)
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> que;
+        vector<vector<int>> result;
+        if (root != nullptr)
+            que.emplace(root);
+
+        while (!que.empty()){
+            vector<int> level;
+            int size = que.size();
+            for (int i=0; i < size; i++){
+                auto front = que.front();
+                que.pop();
+                level.emplace_back(front->val);
+
+                if (front->left != nullptr)
+                    que.emplace(front->left);
+                if (front->right != nullptr)
+                    que.emplace(front->right);
+
+            }
+            result.emplace_back(move(level));
+        }
+        return result;
+    }
+};
---
+++ Python/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,28 @@
+# faster solution than c++ one (less operations)
+# Time:  O(n)
+# Space: O(n)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        result, curr_lev = [], [root]
+        # vals contain values of visited nodes
+        while curr_lev:
+            next_lev, vals = [], []
+            for node in curr_lev:
+                vals.append(node.val)
+                if node.left:
+                    next_lev.append(node.left)
+                if node.right:
+                    next_lev.append(node.right)
+            curr_lev = next_lev
+            result.append(vals)
+
+        return result
