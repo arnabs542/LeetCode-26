12cf6415022b0ea2643f3f6c383850cf9fbe336b
harshit <harshitsharmajs@gmail.com>
2020-05-24 01:07:20 -0700

    0048

---
+++ C++/0048-rotate-image.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+
+        // main-diagonal mirror
+        for (int i = 0; i < n; ++i)
+            for (int j = i; j < n; ++j)
+                swap(matrix[i][j], matrix[j][i]);
+
+        // vertical mirror
+        for (int i=0; i < n; ++i)
+            for (int j=0; j < n/2; ++j)
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+    }
+};
