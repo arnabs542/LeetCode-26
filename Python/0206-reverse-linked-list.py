586871d7879e4a35129f56512075c66718adf170
Harshit Sharma <harshitsharmajs@gmail.com>
2020-06-29 17:26:30 -0700

    Update 0206

--- Python/0206-reverse-linked-list.py
+++ Python/0206-reverse-linked-list.py
@@ -21,11 +21,19 @@ class Solution:
 # recursive
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
+        # not head is for empty list
         if not (head) or not (head.next):
             return head
 
         newHead = self.reverseList(head.next)
+        # we correct pointers while returning
+        # next elem point to head/cur
         head.next.next = head
+        # head/cur points to None
         head.next = None
-
+        # the end of ll gives new_head and is
+        # used by the start of ll
         return newHead
+
+Notes:
+head represents current node.
