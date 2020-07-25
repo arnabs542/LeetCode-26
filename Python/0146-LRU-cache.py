5f72500f06d12c28617547f663748d89bb70000b
Harshit Sharma <harshitsharmajs@gmail.com>
2020-07-24 18:58:06 -0700

    0146

---
+++ Python/0146-LRU-cache.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+
+    def get(self, key: int) -> int:
+        if key in self.dic:
+            node = self.dic[key]
+            self._remove(node)
+            self._add(node)
+            return node.val
+        return -1
+
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            self._remove(node)
+        new = Node(key, value)
+        self._add(new)
+        self.dic[key] = new
+        if len(self.dic) > self.capacity:
+            node = self.head.next
+            self._remove(node)
+            del self.dic[node.key]
+
+
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        node.prev = p
+        node.next = self.tail
+        self.tail.prev = node
+
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
