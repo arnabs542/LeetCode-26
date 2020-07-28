653bb995cccd58d95fe8f567100d7399764c21f2
Harshit Sharma <harshitsharmajs@gmail.com>
2020-07-27 23:17:37 -0700

    0438

---
+++ Python/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,28 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        result = []
+        counter = collections.Counter(p)
+        left = 0
+        for right in range(len(s)):
+            counter[s[right]] -= 1
+
+            while left <= right and counter[s[right]] < 0:
+                # if illegal char
+                # restore the subtracted count
+                counter[s[left]] += 1
+                left += 1
+                # when exit left = right + 1
+
+            if right - left + 1 == len(p):
+                result.append(left)
+
+        return result
+
+# Notes:
+# counter could also be implemented as:
+# cnts = [0] * 26
+# for c in p:
+#     cnts[ord(c) - ord('a')] += 1
