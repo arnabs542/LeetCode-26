470462541840c2e60f017db9165749f3b5d71907
Harshit Sharma <harshitsharmajs@gmail.com>
2020-08-08 02:50:25 -0700

    0621

---
+++ Python/0621-task-schedular.py
@@ -0,0 +1,28 @@
+# Tags: Greedy, Array
+# Time:  O(n)
+# Space: O(26) = O(1)
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        char_map = [0] * 26
+        for c in tasks:
+            char_map[ord(c) - ord('A')] += 1
+
+        char_map.sort(reverse=True)
+        # dont need to wait on the very last occurence
+        max_val = char_map[0] - 1
+        # imagine every task we have to do after max val
+        # task as idle slot
+        idle_slots = max_val * n
+
+        # fill those idle_slots with actual number
+        for i in range(1, len(char_map)):
+            idle_slots -= min(max_val, char_map[i])
+            # in case there are more than one char
+            # with max freq
+            # as in that case, one elem will be outside
+            # the idle slots
+
+        # if we have more chars than idle_slots, no need
+        # to put idle slots
+        return idle_slots + len(tasks) if idle_slots > 0 else\
+    len(tasks)
