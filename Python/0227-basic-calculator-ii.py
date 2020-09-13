c0b89f6c8d7a50dd0f598dee0cb0aa5e473a242b
Harshit Sharma <harshitsharmajs@gmail.com>
2020-09-13 02:57:33 -0700

    0227

---
+++ Python/0227-basic-calculator-ii.py
@@ -0,0 +1,32 @@
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def calculate(self, s: str) -> int:
+        num, stack, sign = 0, [], "+"
+        for i in range(len(s)):
+            if s[i].isdigit():
+                # if we see digit, we update num (single or multi-digit number)
+                num = num*10 + ord(s[i]) - ord("0")
+            if s[i] in "+-/*" or (len(s)-1 == i):
+                # if we see a new operator or it is last char, we
+                # perform operation using the previous
+                # operator and push it into the stack
+                if sign == "+":
+                    stack.append(num)
+                elif sign == "-":
+                    stack.append(-num)
+                elif sign == "*":
+                    stack.append(stack.pop()*num)
+                else:
+                    stack.append(int(stack.pop()/num))
+                    # can use // (floor) as -1.5 becomes -2 instead of -1
+                # reset num
+                num = 0
+                # update sign
+                sign = s[i]
+        return sum(stack)
+
+"""
+We delay addition/subtraction to a later time and only do multiplication/division
+while iterating the string, which helps us in implementating DMAS
+"""
