311df39ba561d9e3f1e9fa56689dcece870216f2
Harshit Sharma <harshitsharmajs@gmail.com>
2020-10-03 19:23:05 -0700

    Update 0070

--- Python/0070-climbing-stairs.py
+++ Python/0070-climbing-stairs.py
@@ -6,7 +6,7 @@ class Solution:
         # base case
         prev, current = 0, 1
         # runs n times
-        for i in range(n):
+        for _ in range(n):
             prev, current = current, prev + current
 
         return current
