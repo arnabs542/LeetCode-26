9924bb38a518146306546dca63074b49bb7151f0
Harshit Sharma <harshitsharmajs@gmail.com>
2020-10-13 18:05:52 -0700

    Update 0079

--- C++/0079-word-search.cpp
+++ C++/0079-word-search.cpp
@@ -36,3 +36,42 @@ public:
 
     }
 };
+
+
+// Time:  O(m * n * l)
+// Space: O(l) where l = length of word, due to recursion
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); ++i)
+            for (int j = 0; j < board[0].size(); ++j)
+                if (dfs(board, word, i, j, 0))
+                    return true;
+        return false;
+    }
+
+    bool dfs(vector<vector<char>> &board, string word, int i, int j, int cur_pos)
+    {
+        if (cur_pos == word.length())
+            return true;
+
+        if (i < 0 || i > board.size() - 1 || j < 0 || j > board[0].size() - 1)
+            return false;
+
+        if (word[cur_pos] != board[i][j])
+            return false;
+
+        board[i][j] = '#';
+
+        vector<pair<int, int>> iters {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (auto &iter: iters) {
+            if (dfs(board, word, i + iter.first, j + iter.second, cur_pos + 1))
+                return true;
+        }
+
+        board[i][j] = word[cur_pos];
+
+        return false;
+    }
+};
