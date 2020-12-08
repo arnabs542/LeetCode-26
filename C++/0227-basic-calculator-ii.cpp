0d2b6bf3316f4a94aef49b4424255e839a7b03ea
Harshit Sharma <harshitsharmajs@gmail.com>
2020-12-07 22:35:43 -0800

    Update 0227

---
+++ C++/0227-basic-calculator-ii.cpp
@@ -0,0 +1,41 @@
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    int calculate(string s) {
+        int result = 0, num = 0;
+        char sign = '+';
+        vector<int> stack;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s[i];
+            if (isdigit(c)) {
+                num = num*10 + (c - '0');
+            } 
+            // if we see a new operator or it is last char, we
+            // perform operation using the previous
+            // operator and push into stack
+            if (!isdigit(c) && !isspace(c) ||  i == s.length() - 1) {
+                if (sign == '+') {
+                    stack.push_back(num);
+                } else if (sign == '-') {
+                    stack.push_back(-num);
+                } else if (sign == '*') {
+                    int num2 = stack.back(); stack.pop_back();
+                    stack.push_back(num * num2);
+                } else if (sign == '/') {
+                    int num2 = stack.back(); stack.pop_back();
+                    stack.push_back(num2 / num);
+                }
+                sign = c;
+                num = 0;
+            }
+        }
+        return accumulate(stack.begin(), stack.end(), 0);
+    }
+};
+
+/*
+Unlike basic calculator (0224) here,
+we delay addition/subtraction to a later time and only do multiplication/division
+while iterating the string, which helps us in implementating DMAS
+*/
