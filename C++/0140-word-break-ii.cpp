25d2340825f9db96c0d38fac1fdaa43e76053681
Harshit Sharma <harshitsharmajs@gmail.com>
2020-12-11 20:05:45 -0800

    0140

---
+++ C++/0140-word-break-ii.cpp
@@ -0,0 +1,32 @@
+// Tags: Amazon DP Backtracking
+// Time: O(s.size() * (avg list size))
+// Space: O(s.size() * (avg list size * avg string size)
+// Memoization solution
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        unordered_map<int, vector<string>> dp {{s.size(), {""}}};
+
+        // returns a list of all sentences that can be built from 
+        // suffix s[i:]
+        // return type: vector<string>
+        // argument types: int
+        function<vector<string>(int)> sentences = [&] (int i) {
+            if (!dp.count(i)) {
+                for (int j = i + 1; j <= s.size(); ++j) {
+                    if (dict.count(s.substr(i, j - i))) {
+                        // j points to the very next index after a
+                        // word is found
+                        for (string tail: sentences(j)) {
+                            dp[i].push_back(s.substr(i, j - i) + (tail == "" ? "" : ' ' + tail));
+                        }
+                    }
+                }
+            }
+            return dp[i];
+        };
+
+        return sentences(0);
+    }
+};
