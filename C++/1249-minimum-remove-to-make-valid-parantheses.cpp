a9f9a6bd2fbf6670a376d527601375092185f83c
Harshit Sharma <harshitsharmajs@gmail.com>
2020-12-30 00:00:06 -0800

    1249

---
+++ C++/1249-minimum-remove-to-make-valid-parantheses.cpp
@@ -0,0 +1,24 @@
+// Tags: String Stack Tiktok
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for (auto i = 0; i < s.length(); ++i) {
+            if (s[i] == '(')
+                st.push(i);
+            if (s[i] == ')') {
+                if (!st.empty())
+                    st.pop();
+                else
+                    s[i] = '*';
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '*'; st.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};
