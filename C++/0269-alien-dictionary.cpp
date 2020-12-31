0682aff25148743ccd5c9440beb0ef3bca8ea7e1
Harshit Sharma <harshitsharmajs@gmail.com>
2020-12-31 02:10:49 -0800

    0269

--- C++/0269-alien-dictionary.cpp
+++ C++/0269-alien-dictionary.cpp
@@ -2,6 +2,7 @@
 // Time:  O(n), n be the total length of all the words
 // in the input list, added together
 // Space: O(|V|+|E|) = O(26 + 26^2) = O(1)
+// Using BFS (Kahn's algorithm)
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
@@ -51,3 +52,76 @@ public:
         return res.length() == indegree.size() ? res : "";
     }
 };
+
+
+
+// Time:  O(n), n be the total length of all the words
+// in the input list, added together
+// Space: O(|V|+|E|) = O(26 + 26^2) = O(1)
+// Using DFS (finding cycle)
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        // 26 characters
+        unordered_map<char, unordered_set<char>> graph;
+        // 26 characters
+        vector<int> visit(26, -2);
+        
+        // initialize
+        for (auto i = 0; i < words.size(); ++i)
+            for (auto j = 0; j < words[i].size(); ++j) {
+                char c = words[i][j];
+                visit[c - 'a'] = 0;
+        }
+        
+        // build graph and record indegree
+        for (auto i = 0; i < words.size() - 1; ++i) {
+            string cur = words[i];
+            string nex = words[i + 1];
+            for (int j = 0; j < min(cur.size(), nex.size()); ++j) {
+                if (cur[j] != nex[j]) {
+                    if (!graph[cur[j]].count(nex[j])) {
+                        graph[cur[j]].insert(nex[j]);
+                    }
+                    break;
+                }
+                // Check that nex is not a prefix of cur 
+                // (invalid input as in valid alphabet, prefix always comes first)
+                if (j == nex.size() - 1 && cur.size() > nex.size())
+                    return "";
+            }
+        }
+        string res;
+        function<bool(char)> dfs = [&] (char u) {
+            if (visit[u - 'a'] == -1)
+                return false;
+            if (visit[u - 'a'] == 1)
+                return true;
+            visit[u - 'a'] = -1;
+            for (auto v: graph[u])
+                if (!dfs(v))
+                    return false;
+            res += u;
+            visit[u - 'a'] = 1;
+            return true;
+        };
+        
+        for (int i = 0; i < 26; ++i)
+            if (visit[i] == 0)
+                if (!dfs('a' + i))
+                    return "";
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+/*
+A topological ordering is possible if and only if the graph has no directed cycles
+
+States:
+visit[i] = -2. Not even exist.
+visit[i] = 0. Exist. Non-visited.
+visit[i] = -1. Visiting.
+visit[i] = 1. Visited.
+*/
