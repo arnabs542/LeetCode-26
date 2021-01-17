275cf7c4ed41a5ff784eb00518e70ed09d9b7f86
Harshit Sharma <harshitsharmajs@gmail.com>
2021-01-17 03:34:57 -0800

    -159

---
+++ C++/0159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -0,0 +1,22 @@
+// Time: O(n)
+// Space: O(1)
+// Same as 0340-longest-substring-with-at-most-k-distinct-characters
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        const int k = 2;
+        int res = 0, distinct = 0;
+        array<int, 256> cnt {0};
+        for (int left = 0, right = 0; right < s.length(); ++right) {
+            if (cnt[s[right]]++ == 0)
+                ++distinct;
+            while (distinct > k) {
+                if(--cnt[s[left]] == 0)
+                    --distinct;
+                ++left;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
