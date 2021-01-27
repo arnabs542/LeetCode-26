7315720cb004369a013ce4d03ea06c063afc6473
Harshit Sharma <harshitsharmajs@gmail.com>
2021-01-27 15:51:26 -0800

    0277

---
+++ C++/0277-find-the-celebrity.cpp
@@ -0,0 +1,33 @@
+// Tags: Array Amazon
+// Time: O(n)
+// Space: O(1)
+class Solution {
+public:
+    int findCelebrity(int n) {
+        // first pass: finding candidate
+        // assume 0 is celebrity
+        int candidate = 0;
+        for (int i = 1; i < n; ++i) {
+            // if candidate knows i, it is not a celebrity
+            if (knows(candidate, i)){
+                // so i could be celebrity
+                candidate = i;
+            }
+        }
+        // second pass: verifying if candidate found is actually
+        // a celebrity
+        for (int i = 0; i < n; ++i) {
+            // if candidate knows someone or someone doesn't know candidate
+            // it is not a celebrity
+            if (i != candidate && (knows(candidate, i) || !knows(i, candidate)))
+                return -1;
+        }
+        return candidate;
+    }
+};
+
+/*
+if candidate is celebrity, 
+    !knows(candidate, 0 to n - 1), he knows no one
+    and knows(0 to n - 1, candidates), everyone knows him
+*/
