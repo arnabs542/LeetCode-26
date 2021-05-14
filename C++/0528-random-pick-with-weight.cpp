5c49acf3d3ede2bc634cd8b0fc01852c5df9b0ff
harshit <harshitsharmajs@gmail.com>
2021-05-13 18:58:11 -0700

    Update

--- C++/0528-random-pick-with-weight.cpp
+++ C++/0528-random-pick-with-weight.cpp
@@ -1,38 +1,55 @@
-// Time:  create prefix_sum: O(n)
-//        pickIndex: O(logn)
+// Tags: Binary-search Random Google
+// Time: create prefix_sum: O(n)
+//       pickIndex: O(logn)
 // Space: O(n)
 class Solution {
-private:
     vector<int> prefix_sum;
 public:
     Solution(vector<int>& w) {
-
-    for (int weight: w) {
-        if (prefix_sum.empty())
-            prefix_sum.push_back(weight);
-        else
-            prefix_sum.push_back(prefix_sum.back() + weight);
+        // prefix_sum[i] = prefix_sum[i - 1] + w[i]
+        partial_sum(w.begin(), w.end(), back_inserter(prefix_sum), plus<int>());
     }
-
-    }
-
+    
     int pickIndex() {
-        int x = prefix_sum.back();
-        int random_num = rand() % x;
-        auto it = upper_bound(prefix_sum.begin(), prefix_sum.end(), random_num);
+        // + 1 as we want to get a value in the [1, prefix_sum.back()]
+        // instead of [0, prefix_sum.back() - 1]
+        int idx = rand() % prefix_sum.back() + 1;
+        // search for first number >= idx
+        auto it = lower_bound(prefix_sum.begin(), prefix_sum.end(), idx);
+        // or upper_bound(prefix_sum.begin(), prefix_sum.end(), rand() % prefix_sum.back());
         return it - prefix_sum.begin();
     }
 };
+/*
+Instead of having an array with w[i] instances of any index i, we can create a prefix sum
+array and later perform a binary search to pick and index, maintaing the probabilities.
+*/
 
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution* obj = new Solution(w);
- * int param_1 = obj->pickIndex();
- */
- /*
- Notes:
- upper_bound - Returns an iterator pointing to the first element
-              in the range [first,last) which compares
-              greater than val.
-lower_bound - ......does not compare less than val
+
+
+// Brute force, time limit exceeded and also takes too much memory
+// Time: O(w[i]*w.length) for constructor, O(1) for pickIndex()
+// Space: O(w[i]*w.length)
+class Solution {
+    vector<int> s;
+public:
+    Solution(vector<int>& w) {
+        int i = 0;
+        for (int x : w) {
+            int t = 0;
+            while (t < x) {
+                s.push_back(i);
+                ++t;
+            }
+            ++i;
+        }
+    }
+    
+    int pickIndex() {
+        return s[rand() % s.size()];
+    }
+};
+/*
+The brute force approach is to have as many instances of i as w[i] in order to get
+the required probability when indexing the resultant array with a random number.
 */
