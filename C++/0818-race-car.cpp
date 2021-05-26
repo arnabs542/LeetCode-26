1ae77695a8461fce68ac1423482ca0c340dce3d5
Harshit Sharma <harshitsharmajs@gmail.com>
2021-05-26 01:38:18 -0700

    1483

--- C++/0818-race-car.cpp
+++ C++/0818-race-car.cpp
@@ -1,12 +1,122 @@
-
-
+// Tags: DP Google
 /*
 Time: O(2^n)
-Naive Bfs solution
+Naive Bfs
 For each position we have two choices - either accelerate or reverse. So we can keep tack of
 all possible positions of the racecar after n instructions and return the smallest n such that
 the target position is reached.
 
 Futher observations reveal that there may be overlapping among intermediate states so we can
 memoize visited state (each state is characterized by car position and speed.)
+*/
+
+
+
+// Time: O(target*log(target)), in worst case all positions in range [0, 2*target] will be
+//       visited and for each position, there can be as many as log(2*target) different speeds
+// Space: O(target*log(target))
+// Optimized Bfs
+class Solution {
+public:
+    int racecar(int target) {
+        // starts from position 0 with speed 1
+        queue<pair<int, int>> q({{0, 1}});
+        unordered_set<string> visited;
+        visited.insert(to_string(0) + " " + to_string(1));
+        
+        for (int level = 0; !q.empty(); ++level) {
+            for (int sz = q.size(); sz > 0; --sz) {
+                auto [pos, speed] = q.front(); q.pop();
+                if (pos == target)
+                    return level;
+                
+                // 'A' instruction
+                pair<int, int> next = {pos + speed, speed*2};
+                string state = to_string(next.first) + " " + to_string(next.second);
+                
+                if (!visited.count(state) && 0 < next.first && next.first < (target << 1)) {
+                    q.push(next);
+                    visited.insert(state);
+                }
+                
+                // 'R' instruction
+                next = {pos, speed > 0 ? -1 : 1};
+                state = to_string(next.first) + " " + to_string(next.second);
+                if (!visited.count(state) && 0 < next.first && next.first < (target << 1)) {
+                    q.push(next);
+                    visited.insert(state);
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+// Time: O(target * (log(target))^2), for each i, m & q will log(i) different values each
+// Space: O(target)
+// Dp solution
+class Solution {
+    vector<int> memo;
+    int dp(int i) {
+        if (memo[i] != INT_MAX)
+            return memo[i];
+        // Note: if number of 'A's is m and initial position is 0, then final position is 2*m - 1
+        // Case 1:
+        // suppose m number of 'A' before first 'R'
+        int j, p, m;
+        for (m = 1; (j = (1 << m) - 1) < i; ++m) {
+            for (int q = 0; (p = (1 << q) - 1) < j; ++q) {
+                memo[i] = min(memo[i], m + 1 + q + 1 + dp(i - (j - p)));
+            }
+        }
+        // m reaches highest value and
+        // j becomes either equal to i or > i (next power of 2 - 1)
+        // Case 2 or 3:
+        memo[i] = min(memo[i], m + (i == j ? 0 : 1 + dp(j - i)));
+        return memo[i]; 
+    }
+public:
+    int racecar(int target) {
+        memo.assign(target + 1, INT_MAX);
+        return dp(target);
+    }
+};
+/*
+We notice that after each reverse, the car's speed returns to 1 or -1. So we can redefine the
+problem in terms of the position of the car only while leaving out the speed.
+
+dp[i] = length of the shortest instructions to move car from position 0 to position i, with
+initial speed of 1 and its direction pointing towards position i.
+
+By this definition, we can only apply dp[i] right after 'R' instruction and we need to make
+sure the direction of initial speed is pointing towards the final target position.
+
+We don't really know how many 'A' instructions there should be before the 'R' instruction, so
+theoretically we need to try all possible cases: 0 A, 1 A, 2 A's, 3 A's and so on. for each
+case, we can obtain the position of the car right before 'R' instruction, which will be
+denoted as j = 2^m - 1, with m being the number of A's.
+
+Depending on relation between i and j, there will be 3 cases:
+1. j < i: car moves in the direction i then at jth position starts moving in reverse direction,
+   then finally at j - p th position starts moving in the direction of i to reach ith position.
+
+   Again, we don't really know how many 'A' instruction there should be in between these 2 'R'
+   instructions, so we try eahc of the cases: 0 A, 1 A, 2 A's, 3 A's and so on.
+   Assumer number of A is q. => p = 2^q - 1
+   total len = m + 1 + q + 1 + dp[i - (j - p)]
+   (both start and target position shifted so that start position is aligned with 0.)
+
+2. j == i: target (ith) position is reached without any 'R' instructions.
+   total len = m
+
+3. j > i: 'R' instruction is issued after the car goes beyond i.
+   total len = m + 1 + dp[j - i]
+   (start and target positions shifted then swapped)
+
+dp[i] = min(above 3 cases)
+
+Base case:
+dp[0] = 0
 */
---
+++ C++/1483-kth-ancestor-of-a-tree-node.cpp
@@ -0,0 +1,80 @@
+// Tags: Google
+/*
+Brute force
+Time: O(q*k), where q = number of queries
+
+What if we remember all kth (1st, 2nd, 3rd, 4th.....) ancestors for each node?
+Time: ctor: O(n*k^2), n = number of nodes
+      getKthAncestor: O(1)
+Still lead to TLE.
+*/
+
+
+
+// Time: ctor: O(log(max(k)) * n), where n = number of nodes
+//       getKthAncestor: O(log(k))
+// Space: O(log(max(k)) * n)
+// Binary lifting / Jump pointers
+class TreeAncestor {
+    vector<vector<int>> up;
+    int LOG;
+public:
+    TreeAncestor(int n, vector<int>& parent) {
+        LOG = 0;
+        // calculate log(n), maximum depth of tree if one sided,
+        // maximum value of k
+        while ((1 << LOG) <= n) {
+            ++LOG;
+        }
+        // LOG = 16
+        up = vector<vector<int>>(n, vector<int>(LOG, -1));
+        
+        // 2^0 = 1st ancestor
+        for (int v = 0; v < n; ++v)
+            up[v][0] = parent[v];
+        
+        
+        // 2^i = 2, 4, 8, 16th ancestor
+        for (int i = 1; i < LOG; ++i) {
+            for (int v = 0; v < n; ++v) {
+                // use k/2th ancestor of k/2th ancestor of v
+                // to calculate kth ancestor of v, k = 2^i
+                if (up[v][i - 1] != -1)
+                    up[v][i] = up[up[v][i - 1]][i - 1];
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        for (int i = 0; i < LOG; ++i) {
+            // ith bit is set
+            if (k & (1 << i)) {
+                // next ancestor to look
+                node = up[node][i];
+                if (node == -1)
+                    return -1;
+            }
+        }
+        return node;
+    }
+};
+/*
+We need a much more efficient way to prepare and get data. When we want to get kth ancestor
+of [node], if we have already known k/2th ancestor, we can use k/2th parent twice to get
+the answer.
+
+Let's remember only 2^ith parent of every node.
+We can get node u's 5th parent by tracing node v's 4th parent, where node v is 1st parent
+of node u.
+
+Let up[u][i] represent node u's 2^ith ancestor. Now, when we want to get kth parent, we just turn
+the number 'k' into binary and check the set bits to trace parent.
+
+Preparing data:
+for v = 0.....n - 1
+up[v][0] = parent[v]
+up[v][1] = up[ up[v][0] ][0]
+up[v][2] = up[ up[v][1] ][1]
+up[v][3] = up[ up[v][2] ][2]
+....
+*/
